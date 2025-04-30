9838237a061e979f19aa830ed9e1b137b00f6c56
lrbassanesi.develcode <lrbassanesi@rd.com.br>
2025-04-30 09:29:03 -0300

    feat: home page spacing

--- src/components/AudioRecorder/styles.ts
+++ src/components/AudioRecorder/styles.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const RecordButton = styled.button`
   background-color: #c95792;
-  align-items: center;
-  justify-content: center;
   padding: 100px;
   border-radius: 100%;
   border-color: transparent;
@@ -21,20 +19,19 @@ export const Container = styled.div`
   width: 100vw;
   height: 70vh;
   align-self: flex-start;
-  justify-content: space-around;
+  justify-content: center;
+  gap: 2rem;
 `;
 
 export const TextSection = styled.div`
   width: 40%;
   box-sizing: border-box;
-  background-color: rgb(256, 256, 256, 0.6);
+  background-color: rgb(256, 256, 256, 0.5);
 `;
 
 export const AudioSection = styled.div`
-  align-items: center;
-  justify-content: center;
+  align-items: end;
+  justify-content: end;
   width: 40%;
   box-sizing: border-box;
-  padding-top: 5rem;
-  padding-bottom: 5rem;
 `;
