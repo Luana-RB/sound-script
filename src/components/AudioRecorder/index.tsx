f74524936d7bfae17f415e4559c689a80f005b1c
lrbassanesi.develcode <lrbassanesi@rd.com.br>
2025-04-30 09:28:46 -0300

    fix: blob format

--- src/components/AudioRecorder/index.tsx
+++ src/components/AudioRecorder/index.tsx
@@ -31,11 +31,12 @@ const AudioRecorder = () => {
 
   const handleStop = async () => {
     const blob = new Blob(audio.current, { type: "audio/webm" });
-
     const formData = new FormData();
-    formData.append("file", blob, "audio/webm");
+    formData.append("file", blob, "audio.webm");
     formData.append("model", "whisper-1");
+    formData.append("language", "pt");
     const openaiKey = import.meta.env.VITE_OPENAI_KEY;
+    console.log("OpenAI Key:", openaiKey);
     try {
       const response = await axios.post(
         "https://api.openai.com/v1/audio/transcriptions",
@@ -50,9 +51,7 @@ const AudioRecorder = () => {
       setTranscript(response.data.text);
     } catch (e) {
       console.error(e);
-      setTranscript(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Vestibulum auctor euismod turpis, vitae tempor lectus consectetur sed. In nec leo magna. Phasellus sollicitudin, nunc sed maximus tempus, nunc dui varius ipsum, a interdum elit turpis ac risus. Curabitur ultricies auctor justo, non sodales purus feugiat non. Nam efficitur libero at purus feugiat, ac viverra neque fermentum. Fusce venenatis eget felis eget pharetra. Nulla facilisi. Aliquam vehicula purus id nisi fermentum, et venenatis dui tempus. Integer ac dictum augue. Vivamus eget placerat tortor.Mauris in odio nec turpis pretium tincidunt. Sed tincidunt eros vel nibh convallis, nec mollis orci viverra. Integer sit amet purus volutpat, faucibus lorem sit amet, sodales nunc. Etiam lobortis lectus eget ligula sollicitudin auctor. Cras fermentum nunc orci, vel aliquet eros scelerisque vel. Nulla facilisi. Morbi convallis odio vel justo rutrum, sit amet gravida felis ultricies. "
-      );
+      setTranscript("Erro na transcrição, verifique sua internet ou API-Key.");
     }
   };
 
