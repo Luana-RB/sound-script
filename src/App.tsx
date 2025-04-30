683748a60cbdd71d6aae5288db2829aa004e37f0
lrbassanesi.develcode <lrbassanesi@rd.com.br>
2025-04-30 09:28:19 -0300

    feat: split background

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import AudioRecorder from "./components/AudioRecorder";
 
 function App() {
   return (
-    <>
-      <h1>Sound Script</h1>
-      <AudioRecorder />
-    </>
+    <div className="app-container">
+      <div className="background-split">
+        <div className="left-half" />
+        <div className="right-half" />
+      </div>
+      <div className="content">
+        <div className="title">
+          <h1 className="first-title">Sound</h1>
+          <h1 className="second-title">Script</h1>
+        </div>
+        <AudioRecorder />
+      </div>
+    </div>
   );
 }
 
